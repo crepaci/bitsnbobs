e2f4719c1a10335d3b0a936103fee813910ec686
Charlie Repaci <158089874+crepaci@users.noreply.github.com>
2024-07-23 12:54:01 -0600

    add df init, add row

--- codesnippets.R
+++ codesnippets.R
@@ -1,4 +1,14 @@
-  # SEPARATE MODEL COEFFICENTS
+
+# initialize empty data frame with specific dimensions and column names
+# source: https://stackoverflow.com/questions/32712301/create-empty-data-frame-with-column-names-by-assigning-a-string-vector
+df <- data.frame(matrix(ncol = 3, nrow = 0))
+colnames(df) <- c("name", "age", "gender")
+
+# add row to data frame 
+# source: https://stackoverflow.com/questions/28467068/how-to-add-a-row-to-a-data-frame-in-r
+df[nrow(df) + 1,] = c("v1","v2")
+
+# SEPARATE MODEL COEFFICENTS
   beta = c("b.i", "b.x1", "b.x2", "b.c")
   for (i in 1:length(beta)) {
     assign(beta[i], model$coefficients[i])
