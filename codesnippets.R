d44ab15829a3a941490b26910ebb1e363f5e417e
Charlie Repaci <158089874+crepaci@users.noreply.github.com>
2024-09-05 13:43:16 -0600

    added example of NA logical issue

--- codesnippets.R
+++ codesnippets.R
@@ -1,4 +1,32 @@
 
+# NOTE: when there are NAs in a variable, a logical vector will also return NA, seemingly introducing an NA row where it doesn't exist or an NA in a different column
+# I fixed this by grabbing the index of TRUE values only using which()
+id=c(1,2,3,4,5)
+y=c("yes", NA, "no", "yes", "no")
+d = data.frame(id, y)
+d
+#   id    y
+# 1  1  yes
+# 2  2 <NA>
+# 3  3   no
+# 4  4  yes
+# 5  5   no
+d[d$y=="yes", ]
+#    id    y
+# 1   1  yes
+# NA NA <NA>
+# 4   4  yes
+d[d$y=="yes", "id"]
+#  1 NA  4
+d$y=="yes"
+# TRUE  NA  FALSE  TRUE  FALSE
+d[which(d$y=="yes"), ]
+#   id   y
+# 1  1 yes
+# 4  4 yes
+d[which(d$y=="yes"), "id"]
+# 1 4
+
 # initialize empty data frame with specific dimensions and column names
 # source: https://stackoverflow.com/questions/32712301/create-empty-data-frame-with-column-names-by-assigning-a-string-vector
 df <- data.frame(matrix(ncol = 3, nrow = 0))
